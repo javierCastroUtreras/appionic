ed84274c20b7db5b441a482eb8f7bd794ade53df
Javier Castro Utreras <castro.jav@hotmail.com>
2021-12-03 17:09:37 +0100

    ionic primera entrega

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,23 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  
+  slides: {img: string, titulo: string, desc: string}[]=[
+    {
+      img:'/assets/img/hotel.svg',
+      titulo: 'El mejor hotel',
+      desc:'Para ti y los tuyos'
+    }, {
+      img:'/assets/img/clock.svg',
+      titulo: 'Reserva con rapidez',
+      desc:'Reservas en menos de un minuto!'
+    }, {
+      img:'/assets/img/bills.svg',
+      titulo: 'Ahorra dinero',
+      desc:'Los mejores precios garantizado!!'
+    }
+  ]
+
   constructor() {}
 
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
-
+export class Tab3Page implements OnInit {
   constructor() {}
 
+  Hoteles =  [{"nombre": "Hotel W",
+  "llegada": "19/04/2022",
+  "salida": "22/04/2022",
+  "img": "w.jpg",
+  "habitaciones": 1,
+  "adultos": 2,
+  "niños": 0
+},{"nombre": "Hotel Picasso",
+"llegada": "17/08/2022",
+"salida": "21/08/2022",
+"img": "picasso.jpg",
+"habitaciones": 2,
+"adultos": 4,
+"niños": 2
+}];
+
+  ngOnInit() {
+  }
+  remove(i){
+    this.Hoteles.splice(i,1);
+  }
 }
